ec4d89e761428d412c1e0c9f51fe01e979c33888
phamhieu2212 <hieuphamminh96@gmail.com>
2020-05-07 09:53:43 +0700

    update_route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
     return view('pages.admin.v1.layout.application');
 });
 
