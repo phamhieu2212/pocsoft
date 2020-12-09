0ea182f193de8f5f976c6aba121a965ceec9e65f
phamhieu <hieuphamminh96@gmail.com>
2020-12-09 16:31:20 +0700

    image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -407,83 +407,84 @@
     </section><!-- End Team Section -->
 
     <!-- ======= Our Skills Section ======= -->
-    <section id="skills" class="skills">
-        <div class="container">
+{{--    <section id="skills" class="skills">--}}
+{{--        <div class="container">--}}
 
-            <div class="section-title">
-                <h2>Kỹ năng</h2>
-                <p>Kỹ năng của chúng tôi</p>
-            </div>
+{{--            <div class="section-title">--}}
+{{--                <h2>Kỹ năng</h2>--}}
+{{--                <p>Kỹ năng của chúng tôi</p>--}}
+{{--            </div>--}}
 
-            <div class="row skills-content">
+{{--            <div class="row skills-content">--}}
 
-                <div class="col-lg-6">
+{{--                <div class="col-lg-6">--}}
 
-                    <div class="progress">
-                        <span class="skill">HTML <i class="val">100%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">HTML <i class="val">100%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
-                    <div class="progress">
-                        <span class="skill">CSS <i class="val">100%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">CSS <i class="val">100%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
-                    <div class="progress">
-                        <span class="skill">JavaScript <i class="val">75%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <span class="skill">IOS <i class="val">70%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">JavaScript <i class="val">75%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">IOS <i class="val">70%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
-                </div>
+{{--                </div>--}}
 
-                <div class="col-lg-6">
+{{--                <div class="col-lg-6">--}}
 
-                    <div class="progress">
-                        <span class="skill">PHP <i class="val">85%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">PHP <i class="val">85%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
-                    <div class="progress">
-                        <span class="skill">WordPress/CMS <i class="val">70%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">WordPress/CMS <i class="val">70%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
-                    <div class="progress">
-                        <span class="skill">Photoshop <i class="val">80%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <div class="progress">
-                        <span class="skill">Android <i class="val">80%</i></span>
-                        <div class="progress-bar-wrap">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">Photoshop <i class="val">80%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="progress">--}}
+{{--                        <span class="skill">Android <i class="val">80%</i></span>--}}
+{{--                        <div class="progress-bar-wrap">--}}
+{{--                            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
 
-                </div>
+{{--                </div>--}}
 
-            </div>
+{{--            </div>--}}
 
-        </div>
-    </section><!-- End Our Skills Section -->
+{{--        </div>--}}
+{{--    </section>--}}
+    <!-- End Our Skills Section -->
 {{--    <!-- ======= Testimonials Section ======= -->--}}
 {{--    <section id="testimonials" class="testimonials">--}}
 {{--        <div class="container">--}}
