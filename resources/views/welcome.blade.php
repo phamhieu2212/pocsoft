41d0575343faefee2afe62658bb1e05bdf62eb3c
phamhieu <hieuphamminh96@gmail.com>
2020-12-09 16:15:26 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -637,7 +637,7 @@
 </main><!-- End #main -->
 
 <!-- ======= Footer ======= -->
-{{--<footer id="footer">--}}
+<footer id="footer">
 {{--    <div class="footer-top">--}}
 {{--        <div class="container">--}}
 {{--            <div class="row">--}}
@@ -695,19 +695,19 @@
 {{--        </div>--}}
 {{--    </div>--}}
 
-{{--    <div class="container">--}}
-{{--        <div class="copyright">--}}
-{{--            &copy; Copyright <strong><span>POCSOFT</span></strong>. All Rights Reserved--}}
-{{--        </div>--}}
-{{--        <div class="credits">--}}
-{{--            <!-- All the links in the footer should remain intact. -->--}}
-{{--            <!-- You can delete the links only if you purchased the pro version. -->--}}
-{{--            <!-- Licensing information: https://bootstrapmade.com/license/ -->--}}
-{{--            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->--}}
-{{--            Designed by <a href="https://bootstrapmade.com/">POCSOFT</a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</footer>--}}
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>POCSOFT</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->
+            Designed by <a href="https://bootstrapmade.com/">POCSOFT</a>
+        </div>
+    </div>
+</footer>
 <!-- End Footer -->
 
 <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
