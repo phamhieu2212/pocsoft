bdcd6bfe1a4c121cd656160238c45127e30ad8aa
phamhieu <hieuphamminh96@gmail.com>
2020-12-09 13:39:11 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
         <nav class="nav-menu d-none d-lg-block">
 
             <ul>
-                <li class=""><a href="">Trang chủ</a></li>
+                <li class="#hero"><a href="">Trang chủ</a></li>
 
                 <li><a href="#about">Về chúng tôi</a>
                 </li>
